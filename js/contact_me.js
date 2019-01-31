22d940e9e9704fc8f842f9736d0090d8ea4ebbde
ecosse3 <theecosse@gmail.com>
2019-01-31 22:39:15 +0100

    Repaired contact js

--- js/contact_me.js
+++ js/contact_me.js
@@ -10,7 +10,7 @@ $(function() {
       // get values from FORM
       var name = $("input#name").val();
       var email = $("input#email").val();
-      var phone = $("input#phone").val();
+      var need = $("input#need").val();
       var message = $("textarea#message").val();
       var firstName = name; // For Success/Failure Message
       // Check for white space in name for Success/Fail message
@@ -24,7 +24,7 @@ $(function() {
         type: "POST",
         data: {
           name: name,
-          phone: phone,
+          need: need,
           email: email,
           message: message
         },
