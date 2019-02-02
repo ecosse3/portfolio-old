8fb4e44defc05bd56d7fb27be7fddf028fd1a62b
ecosse3 <theecosse@gmail.com>
2019-02-02 16:56:34 +0100

    Changed email

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -18,8 +18,8 @@ $need = strip_tags(htmlspecialchars($_POST['topic']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
-$to = 'markt@vesper.org.pl'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
+$to = 'kontakt@kvsky.pl'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Nowa wiadomosc od kvsky.pl:  $name";
 $email_body = "Nowa wiadomosc od kvsky.pl!\n\n"."Imie: $name\n\nNazwisko: $lastname\n\nEmail: $email_address\n\nTemat: $need\n\nWiadomosc:\n$message";
 $headers = "From: kontakt@kvsky.pl\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
